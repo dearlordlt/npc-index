29e1b450157d714c3d7feeada00c7a1a6872e7db
ssliteris <ssliteris@deloitte.com>
2016-04-03 21:09:04 +0300

    first mock

--- app/js/main/controllers/CreateCtrl.js
+++ app/js/main/controllers/CreateCtrl.js
@@ -1,3 +1,36 @@
 app.controller('CreateCtrl', ['$scope', function($scope) {
 
+    $scope.familyList = [];
+
+    $scope.family = {
+        familyMemberType: '',
+        familyMemberName: ''
+    };
+
+    $scope.resetFamilyForm = function() {
+        $scope.family = {
+            familyMemberType: '',
+            familyMemberName: ''
+        }
+    };
+
+    $scope.resetFamilyForm();
+
+    $scope.test = 'asdasdds';
+
+    $scope.addFamilyMember = function() {
+        var newFamilyMember = {
+            type: $scope.family.familyMemberType,
+            name: $scope.family.familyMemberName
+        };
+
+        $scope.familyList.push(newFamilyMember);
+
+        $scope.resetFamilyForm();
+    };
+
+    $scope.removeFamilyMember = function(i) {
+        $scope.familyList.splice(i, 1);
+    }
+
 }]);
